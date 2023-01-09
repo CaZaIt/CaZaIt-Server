d2a0226798ad3916179f8ba43c778f874453301c
ParkRootSeok <parkrootseok@gmail.com>
2023-01-10 02:40:44 +0900

    [ADD] #5 - 1(User, Cafe) : N(cafeFavorites) 연관관계 매핑

--- src/main/java/shop/cazait/domain/cafefavorites/entity/cafeFavorites.java
+++ src/main/java/shop/cazait/domain/cafefavorites/entity/cafeFavorites.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,8 +24,12 @@ public class cafeFavorites {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne
+    @JoinColumn(name = "cafe_id")
     private Cafe cafe;
 
 }
