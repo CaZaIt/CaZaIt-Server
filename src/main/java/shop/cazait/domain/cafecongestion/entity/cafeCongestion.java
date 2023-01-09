de0df84ee333b599abad89f4a3aa542ed015a159
ParkRootSeok <g_e_un@naver.com>
2023-01-10 01:31:24 +0900

    [ADD] #3 - cafeCongestion 필드 변수 생성

--- src/main/java/shop/cazait/domain/cafecongestion/entity/cafeCongestion.java
+++ src/main/java/shop/cazait/domain/cafecongestion/entity/cafeCongestion.java
@@ -1,7 +1,31 @@
 package shop.cazait.domain.cafecongestion.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.cazait.domain.cafe.entity.Cafe;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class cafeCongestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Cafe cafe;
+
+    @Enumerated(EnumType.STRING)
+    private congestionStatus status;
+
 }
