71cf49be7ab8c827e79c7bcc072b32a46dc101c7
잉퓨 <dayeon620@kakao.com>
2023-01-10 17:30:18 +0900

    [ADD] #12 Master 컬럼, 엔티티 필드변수 생성

--- src/main/java/shop/cazait/domain/master/entity/Master.java
+++ src/main/java/shop/cazait/domain/master/entity/Master.java
@@ -1,7 +1,27 @@
 package shop.cazait.domain.master.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class Master {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private MasterStatus status;
+
 }
