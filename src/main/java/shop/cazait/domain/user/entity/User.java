ff267799963f2e1117fc1226f59ed385e5dd18c0
minjun <ijmtylhtf@naver.com>
2023-01-10 20:12:19 +0900

    [ADD] #18 컬럼과 엔티티 필드 변수 매핑

--- src/main/java/shop/cazait/domain/user/entity/User.java
+++ src/main/java/shop/cazait/domain/user/entity/User.java
@@ -1,8 +1,28 @@
 package shop.cazait.domain.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
 }
 
