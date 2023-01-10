4e26ecffd0d455c3b43903a478fbfa04aad1c2ab
swandevson <shai4956@gmail.com>
2023-01-10 18:24:01 +0900

    [ADD] #15 - Entity 연관관계 매핑

--- src/main/java/shop/cazait/domain/review/entity/Review.java
+++ src/main/java/shop/cazait/domain/review/entity/Review.java
@@ -1,16 +1,20 @@
 package shop.cazait.domain.review.entity;
 
-import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import shop.cazait.domain.cafe.entity.Cafe;
+import shop.cazait.domain.user.entity.User;
+
 
 
 @Entity
@@ -24,16 +28,19 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NonNull
-    private long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @Column(nullable = false)
+    private User user;
 
-    @NonNull
-    private long cafeId;
+    @ManyToOne
+    @JoinColumn(name = "cafe_id")
+    @Column(nullable = false)
+    private Cafe cafe;
 
-    @NonNull
+    @Column(nullable = false)
     private int score;
 
-    @Nullable
     private String content;
 
     /**
