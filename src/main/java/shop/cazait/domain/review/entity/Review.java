e366143bcb381f517312be9d39f6b8cce80987a8
swandevson <shai4956@gmail.com>
2023-01-10 18:08:57 +0900

    [ADD] #15 Entity 변수 생성

--- src/main/java/shop/cazait/domain/review/entity/Review.java
+++ src/main/java/shop/cazait/domain/review/entity/Review.java
@@ -1,7 +1,46 @@
 package shop.cazait.domain.review.entity;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+@Getter
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NonNull
+    private long userId;
+
+    @NonNull
+    private long cafeId;
+
+    @NonNull
+    private int score;
+
+    @Nullable
+    private String content;
+
+    /**
+     * 추후 확장
+     *@NonNull
+     *private int seat;
+     *@NonNull
+     *private int cleanliness;
+     */
 }
