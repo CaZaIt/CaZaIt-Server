a22622ec43e89246f371ac9b2383dca511578533
minjun <ijmtylhtf@naver.com>
2023-01-10 20:18:05 +0900

    [ADD] #18 Nullable

--- src/main/java/shop/cazait/domain/user/entity/User.java
+++ src/main/java/shop/cazait/domain/user/entity/User.java
@@ -1,9 +1,6 @@
 package shop.cazait.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,10 +17,13 @@ public class User extends baseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String nickname;
 }
 
