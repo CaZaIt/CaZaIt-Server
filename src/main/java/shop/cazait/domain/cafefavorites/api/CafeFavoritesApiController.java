4dc311f665852fe4fa9d50579c8adc747e41ae0d
ParkRootSeok <parkrootseok@gmail.com>
2023-01-13 00:36:56 +0900

    [Feat] #31 - Swagger API 설정 추가

--- src/main/java/shop/cazait/domain/cafefavorites/api/CafeFavoritesApiController.java
+++ src/main/java/shop/cazait/domain/cafefavorites/api/CafeFavoritesApiController.java
@@ -1,5 +1,10 @@
 package shop.cazait.domain.cafefavorites.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,12 +13,18 @@ import shop.cazait.domain.cafefavorites.dto.PostCafeFavoritesRes;
 import shop.cazait.domain.cafefavorites.service.CafeFavoritesService;
 import shop.cazait.global.common.response.BaseResponse;
 
+@Api(tags = "즐겨찾기 API")
 @RestController
 @RequiredArgsConstructor
 public class CafeFavoritesApiController {
 
     private final CafeFavoritesService cafeFavoritesService;
 
+    @ApiOperation(value = "즐겨찾기 등록", notes = "사용자 ID와 카페 ID를 받아 즐겨찾기 등록")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "사용자 ID"),
+            @ApiImplicitParam(name = "cafeId", value = "카페 ID")
+    })
     @PostMapping("/user/{userId}/favorites/{cafeId}")
     public BaseResponse<PostCafeFavoritesRes> addCafeFavorites(@PathVariable(name = "userId") Long userId,
                                                               @PathVariable(name = "cafeId") Long cafeId) {
--- src/main/java/shop/cazait/domain/cafefavorites/dto/GetCafeFavoritesRes.java
+++ src/main/java/shop/cazait/domain/cafefavorites/dto/GetCafeFavoritesRes.java
@@ -1,14 +1,20 @@
 package shop.cazait.domain.cafefavorites.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "즐겨찾기 한 카페 정보", description = "카페 ID, 이름, 이미지를 가진 DTO")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetCafeFavoritesRes {
 
+    @ApiModelProperty(value = "카페 ID")
     private Long cafeId;
+    @ApiModelProperty(value = "카페 이름")
     private String name;
+    @ApiModelProperty(value = "카페 이미지")
     private String imageUrl;
 
     @Builder
