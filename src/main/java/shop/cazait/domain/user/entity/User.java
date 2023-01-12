ca0701e21622b599e60eba57f8ae5f4b437ce4ff
minjun <ijmtylhtf@naver.com>
2023-01-12 13:13:21 +0900

    [ADD] #28 - @Builder 사용 방법 변경

--- src/main/java/shop/cazait/domain/user/entity/User.java
+++ src/main/java/shop/cazait/domain/user/entity/User.java
@@ -1,18 +1,17 @@
 package shop.cazait.domain.user.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shop.cazait.global.common.entity.baseEntity;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Getter
-public class User extends baseEntity {
+public class User extends baseEntity{
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,5 +24,12 @@ public class User extends baseEntity {
 
     @Column(nullable = false)
     private String nickname;
+
+    @Builder
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
 
