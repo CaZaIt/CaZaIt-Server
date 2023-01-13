5f416996951af27b887f95e4930c2a4ad73016dc
ParkRootSeok <parkrootseok@gmail.com>
2023-01-13 22:50:49 +0900

    [Refactor] #2 - Spring boot Version(3.0.0 -> 2.7.7) 변경으로 인한 코드 수정

--- src/main/java/shop/cazait/domain/review/entity/Review.java
+++ src/main/java/shop/cazait/domain/review/entity/Review.java
@@ -1,22 +1,13 @@
 package shop.cazait.domain.review.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.persistence.Entity;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import shop.cazait.domain.cafe.entity.Cafe;
 import shop.cazait.domain.user.entity.User;
-
-
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
