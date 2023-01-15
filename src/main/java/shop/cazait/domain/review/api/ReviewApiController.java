31ac70d5d9c590dd820b4b8f53972e31330a72ee
swandevson <shai4956@gmail.com>
2023-01-16 00:05:46 +0900

    [Feat] #37 - 리뷰 목록 조회 swagger 설정 추가

--- src/main/java/shop/cazait/domain/review/api/ReviewApiController.java
+++ src/main/java/shop/cazait/domain/review/api/ReviewApiController.java
@@ -1,20 +1,35 @@
 package shop.cazait.domain.review.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import shop.cazait.domain.review.dto.GetReviewsRes;
 import shop.cazait.domain.review.service.ReviewDaoService;
 import shop.cazait.domain.review.service.ReviewProvideService;
+import shop.cazait.global.common.response.BaseResponse;
 
 
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "리뷰 API")
 @RequestMapping("/app/cafe")
 public class ReviewApiController {
     private final ReviewDaoService reviewDaoService;
     private final ReviewProvideService reviewProvideService;
 
     @ResponseBody
+    @ApiOperation(value = "리뷰 전체 조회", notes = "카페 ID를 받아 해당 카페의 리뷰 목록 및 평점을 반환")
+    @ApiImplicitParam(
+            name = "cafeId",
+            value = "카페 ID"
+    )
     @GetMapping("/{cafeId}/reviews")
     public BaseResponse<GetReviewsRes> getReviews(@PathVariable long cafeId) {
         GetReviewsRes getReviewsRes = reviewProvideService.getReviews(cafeId);
--- src/main/java/shop/cazait/domain/review/dto/GetReviewsRes.java
+++ src/main/java/shop/cazait/domain/review/dto/GetReviewsRes.java
@@ -1,5 +1,7 @@
 package shop.cazait.domain.review.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,9 +11,12 @@ import shop.cazait.domain.review.entity.Review;
 
 
 @Getter
+@ApiModel(value = "리뷰들 및 평점", description = "해당 카페에 등록된 리뷰들 및 카페 평점")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GetReviewsRes {
+    @ApiModelProperty(value = "카페 평점")
     private double averageScore;
+    @ApiModelProperty(value = "카페에 등록된 리뷰들")
     private List<Review> reviews;
 
     @Builder
