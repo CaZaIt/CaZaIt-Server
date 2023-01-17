edfda6e108b75b9c3f3f1b17a9d9af2a3a9c3c91
ParkRootSeok <parkrootseok@gmail.com>
2023-01-18 01:51:08 +0900

    [Add] #2 - 상수 추가

--- src/main/java/shop/cazait/global/common/constant/Constant.java
+++ src/main/java/shop/cazait/global/common/constant/Constant.java
@@ -1,7 +1,10 @@
 package shop.cazait.global.common.constant;
 
+import shop.cazait.domain.congestion.entity.Congestion;
+
 public class Constant {
 
     public static final String NOT_EXIST = null;
+    public static final Congestion CONGESTION_NOT_EXIST = null;
 
 }
