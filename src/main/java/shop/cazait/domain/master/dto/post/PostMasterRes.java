da4a7226caaee244bd8638544128ba126c93a24f
잉퓨 <dayeon620@kakao.com>
2023-01-19 03:28:42 +0900

    [Feat] #27 마스터 계정 반환값 Id 추가

--- src/main/java/shop/cazait/domain/master/dto/post/PostMasterRes.java
+++ src/main/java/shop/cazait/domain/master/dto/post/PostMasterRes.java
@@ -1,5 +1,6 @@
 package shop.cazait.domain.master.dto.post;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +11,13 @@ import shop.cazait.domain.master.entity.Master;
 @Builder
 public class PostMasterRes {
 
+    @ApiModelProperty(value = "마스터 계정 ID")
+    private  Long id;
+
+    @ApiModelProperty(value = "마스터 계정 이메일")
     private  String email;
+
+    @ApiModelProperty(value = "마스터 계정 이름")
     private  String nickname;
 
     static public PostMasterRes toDto(Master master) {
