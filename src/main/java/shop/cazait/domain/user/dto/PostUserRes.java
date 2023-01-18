c94584d624f90b995f921a7373c80a32f0a8a3c5
minjun <ijmtylhtf@naver.com>
2023-01-18 22:13:16 +0900

    [Refactor] #51 - PostUserRes에 name 변수 삭제

--- src/main/java/shop/cazait/domain/user/dto/PostUserRes.java
+++ src/main/java/shop/cazait/domain/user/dto/PostUserRes.java
@@ -1,2 +1,34 @@
-package shop.cazait.domain.user.dto;public class PostUserRes {
+package shop.cazait.domain.user.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.cazait.domain.user.entity.User;
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostUserRes {
+
+    private Long id;
+    private String password;
+    private String email;
+    private String nickname;
+
+    @Builder
+    public PostUserRes(Long id, String email, String password, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public static PostUserRes of(User user) {
+        return PostUserRes.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .password(user.getPassword())
+                .build();
+    }
+
 }
