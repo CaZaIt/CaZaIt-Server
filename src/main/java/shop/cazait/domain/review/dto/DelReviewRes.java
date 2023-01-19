a8bd666e24a3d302cd7d01d4451fe47d11bc0b63
swandevson <shai4956@gmail.com>
2023-01-20 02:07:44 +0900

    [Fix] #37 - 누락된 @Getter 추가
    - @ResponseBody시 json 데이터 변환을 위해 getter가 필요함

--- src/main/java/shop/cazait/domain/review/dto/DelReviewRes.java
+++ src/main/java/shop/cazait/domain/review/dto/DelReviewRes.java
@@ -4,11 +4,13 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import shop.cazait.domain.review.entity.Review;
 
 
 
 @ApiModel(value = "리뷰 삭제 Response DTO", description = "삭제한 리뷰의 ID")
+@Getter
 @Builder(access = AccessLevel.PRIVATE)
 public class DelReviewRes {
     @ApiModelProperty(value = "리뷰 ID")
