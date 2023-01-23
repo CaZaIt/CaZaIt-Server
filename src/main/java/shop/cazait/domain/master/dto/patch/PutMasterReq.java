0e2b935d83050999e594d9e4eb076c936ab8b4b4
잉퓨 <dayeon620@kakao.com>
2023-01-23 18:43:58 +0900

    [Chore] #72 - @Setter 패턴 수정

--- src/main/java/shop/cazait/domain/master/dto/patch/PutMasterReq.java
+++ src/main/java/shop/cazait/domain/master/dto/patch/PutMasterReq.java
@@ -2,12 +2,8 @@ package shop.cazait.domain.master.dto.patch;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class PutMasterReq {
 
 	@ApiModelProperty(value = "Master 이메일")
--- src/main/java/shop/cazait/domain/master/dto/patch/PutMasterRes.java
+++ src/main/java/shop/cazait/domain/master/dto/patch/PutMasterRes.java
@@ -3,10 +3,8 @@ package shop.cazait.domain.master.dto.patch;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class PutMasterRes {
 
