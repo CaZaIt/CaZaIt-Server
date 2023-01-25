7bebb58bc6ae9b83936cff45f9cca2178186a5bf
minjun <ijmtylhtf@naver.com>
2023-01-25 23:11:36 +0900

    [Fix] #84 - JJWT signWith deprecated 오류해결

--- src/main/java/shop/cazait/global/config/encrypt/JwtService.java
+++ src/main/java/shop/cazait/global/config/encrypt/JwtService.java
@@ -5,28 +5,27 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
-import java.security.Signature;
 import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import shop.cazait.global.error.exception.BaseException;
-
 import static shop.cazait.global.error.status.ErrorStatus.EMPTY_JWT;
 import static shop.cazait.global.error.status.ErrorStatus.INVALID_JWT;
 
 @Service
 public class JwtService {
+
     private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     public String createJwt(Long userIdx){
-
+        System.out.println("userIdx = " + userIdx);
         return Jwts.builder()
-                .signWith(key)
-                .setSubject(String.valueOf(userIdx))
-                .setIssuer("GGOMG")
+                .setHeaderParam("type","jwt")
+                .claim("userIdx",userIdx)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(key)
                 .compact();
     }
 
@@ -35,24 +34,27 @@ public class JwtService {
         return request.getHeader("X-ACCESS-TOKEN");
     }
 
-    public int getUserIdx() throws BaseException{
-        //1. JWT 추출
+    public Long getUserIdx() throws BaseException{
+
+        // JWT 추출
         String accessToken = getJwt();
         if(accessToken == null || accessToken.length() == 0){
             throw new BaseException(EMPTY_JWT);
         }
 
-        // 2. JWT parsing
+        // JWT parsing
         Jws<Claims> claims;
-        try{
-            claims = Jwts.parser()
-                    .setSigningKey(Secret.JWT_SECRET_KEY)
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
                     .parseClaimsJws(accessToken);
-        } catch (Exception ignored) {
+        }
+      catch (Exception ignored) {
             throw new BaseException(INVALID_JWT);
         }
 
-        // 3. userIdx 추출
-        return claims.getBody().get("userIdx",Integer.class);  // jwt 에서 userIdx를 추출합니다.
+        // userIdx 추출
+        return claims.getBody().get("userIdx",Long.class);  // jwt 에서 userIdx를 추출합니다.
     }
 }
