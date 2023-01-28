60eef6c76eebc79e972ab5b5f5532adb8f0d5090
minjun <ijmtylhtf@naver.com>
2023-01-28 21:24:49 +0900

    Revert "[Fix] #84 - userId 가져오지 못하는 오류 수정 (저장 먼저 진행)"
    This reverts commit e8185e6f4770799e579c46769105e0730f4b7b4f.

--- src/main/java/shop/cazait/domain/user/service/UserService.java
+++ src/main/java/shop/cazait/domain/user/service/UserService.java
@@ -6,9 +6,7 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.cazait.domain.user.dto.*;
@@ -16,13 +14,16 @@ import shop.cazait.domain.user.entity.User;
 import shop.cazait.domain.user.exception.UserException;
 import shop.cazait.domain.user.repository.UserRepository;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import shop.cazait.global.config.encrypt.AES128;
 import shop.cazait.global.config.encrypt.JwtService;
 import shop.cazait.global.config.encrypt.Secret;
-import shop.cazait.global.error.exception.BaseException;
+import shop.cazait.global.error.status.ErrorStatus;
 
 import static shop.cazait.global.error.status.ErrorStatus.*;
 
@@ -30,7 +31,6 @@ import static shop.cazait.global.error.status.ErrorStatus.*;
 @Service
 @RequiredArgsConstructor
 @Transactional
-@Slf4j
 public class UserService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
@@ -60,9 +60,9 @@ public class UserService {
         pwd = new AES128(Secret.USER_INFO_PASSWORD_KEY).encrypt(postUserReq.getPassword());
         PostUserReq EncryptPostUserReq = new PostUserReq(postUserReq.getEmail(), pwd, postUserReq.getNickname());
         User user = EncryptPostUserReq.toEntity();
-        userRepository.save(user);
         Long userIdx = user.getId();
         String jwt = jwtService.createJwt(userIdx);
+        userRepository.save(user);
         return PostUserRes.of(user,jwt);
     }
 
@@ -111,7 +111,7 @@ public class UserService {
         return GetUserRes.of(findUser);
     }
 
-    public PatchUserRes modifyUser(Long userIdx,PatchUserReq patchUserReq, String refreshToken){
+    public PatchUserRes modifyUser(Long userIdx,PatchUserReq patchUserReq){
         User modifyUser = patchUserReq.toEntity();
         User existUser = userRepository.findById(userIdx).get();
 
@@ -120,7 +120,6 @@ public class UserService {
                 .email(modifyUser.getEmail())
                 .password(modifyUser.getPassword())
                 .nickname(modifyUser.getNickname())
-                .refreshToken(refreshToken)
                 .build();
         userRepository.save(existUser);
         return PatchUserRes.of(existUser);
@@ -130,49 +129,5 @@ public class UserService {
         User deleteUser = userRepository.findByEmail(email).get();
         userRepository.delete(deleteUser);
         return DeleteUserRes.of(deleteUser);
-    }
-
-    public PostLoginRes issueAccessToken(String accessToken,String refreshToken) throws BaseException , UserException{
-        User user = null;
-        Long userIdx = null;
-
-        log.info("accessToken = " + accessToken);
-        log.info("refreshToken = " + refreshToken);
-
-        if(jwtService.isValidAccessToken(accessToken))
-        {
-            log.info("아직 accesstoken 유효");
-            throw new UserException(FAILED_TO_LOGIN);
-        }
-        else
-        {
-            log.info("Access 토큰 만료됨");
-            if(jwtService.isValidRefreshToken(refreshToken)){     //들어온 Refresh 토큰이 유효한지
-                log.info("아직 refreshtoken 유효함");
-                userIdx = jwtService.getUserIdx(accessToken);
-                user = userRepository.findById(userIdx).get();
-                String tokenFromDB = user.getRefreshToken();
-                log.info("userIdx from accessToken: "+userIdx);
-                log.info("refreshToken found by accessToken(userIdx): "+tokenFromDB);
-
-                if(refreshToken.equals(tokenFromDB)) {
-                    log.info("Access token 재발급");
-                    accessToken = jwtService.createJwt(userIdx);
-                }
-                else{
-                    log.error("Refresh Token Tampered, not equal from db refreshtoken");
-                    throw new UserException(FAILED_TO_LOGIN);
-                }
-            }
-            else
-            {
-                log.info("refresh token 재발급");
-                userIdx = jwtService.getUserIdx(accessToken);
-                user = userRepository.findById(userIdx).get();
-                accessToken = jwtService.createJwt(userIdx);
-                refreshToken = jwtService.createRefreshToken();
-            }
-        }
-        return PostLoginRes.of(user,accessToken,refreshToken);
     }
 }
