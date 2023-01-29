210319622fae6e20f1f3d5b56949cc16b3c68dfd
swandevson <shai4956@gmail.com>
2023-01-29 17:24:15 +0900

    [Add] #89 - Review 예외처리 추가

--- src/main/java/shop/cazait/global/error/exception/GlobalExceptionHandler.java
+++ src/main/java/shop/cazait/global/error/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package shop.cazait.global.error.exception;
 
+import java.util.Map;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import shop.cazait.domain.cafe.exception.CafeException;
@@ -7,6 +9,7 @@ import shop.cazait.domain.cafeimage.exception.CafeImageException;
 import shop.cazait.domain.favorites.exception.FavoritesException;
 import shop.cazait.domain.congestion.exception.CongestionException;
 import shop.cazait.domain.master.error.MasterException;
+import shop.cazait.domain.review.exception.ReviewException;
 import shop.cazait.domain.user.exception.UserException;
 import shop.cazait.global.common.response.FailResponse;
 
@@ -45,6 +48,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ UserException.class })
     protected FailResponse handleUserException(UserException exception) {
+        return new FailResponse(exception.getError());
+    }
+
+    @ExceptionHandler({ReviewException.class })
+    protected FailResponse handlerReviewException(ReviewException exception) {
         return new FailResponse(exception.getError());
     }
 }
