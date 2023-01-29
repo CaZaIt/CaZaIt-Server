eb8f36ea91508eb168f55722210eec52d9ca9fc6
Kim Ahyun <kimahyunn132@gmail.com>
2023-01-29 23:44:56 +0900

    [ADD] #86 - Request dto 수정

--- src/main/java/shop/cazait/domain/cafe/dto/PostCafeReq.java
+++ src/main/java/shop/cazait/domain/cafe/dto/PostCafeReq.java
@@ -3,26 +3,23 @@ package shop.cazait.domain.cafe.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
-import shop.cazait.domain.congestion.entity.Congestion;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @ApiModel(value = "카페 정보 등록 및 수정 Request", description = "카페 등록 및 수정 시 필요한 정보")
 @Getter
+@NoArgsConstructor
 public class PostCafeReq {
 
     @ApiModelProperty(value = "이름", example = "롬곡")
     @NotBlank(message = "카페 이름을 입력해주세요.")
-    private final String name;
+    private String name;
     @ApiModelProperty(value = "위치", example = "서울시 중구")
     @NotBlank(message = "카페 위치를 입력해주세요.")
-    private final String location;
+    private String location;
     @ApiModelProperty(value = "경도", example = "127.543215")
-    private final double longitude;
+    private double longitude;
     @ApiModelProperty(value = "위도", example = "36.987561")
-    private final double latitude;
+    private double latitude;
 
     @Builder
     public PostCafeReq(String name, String location, double longitude, double latitude) {
--- src/main/java/shop/cazait/domain/cafeimage/dto/PostCafeImageReq.java
+++ src/main/java/shop/cazait/domain/cafeimage/dto/PostCafeImageReq.java
@@ -1,21 +1,17 @@
 package shop.cazait.domain.cafeimage.dto;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @ApiModel(value = "카페 이미지 Request", description = "카페 이미지 등록 시 필요한 정보")
 @Getter
-@JsonAutoDetect
+@NoArgsConstructor
 public class PostCafeImageReq {
     @ApiModelProperty(value = "카페 이미지 링크", example = "americano.png")
     private String imageUrl;
-
-    public PostCafeImageReq() {
-
-    }
 
     @Builder
     public PostCafeImageReq(String imageUrl) {
