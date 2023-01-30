bd0eb729e6d256ad6155d2506a26fa812a77b630
minjun <ijmtylhtf@naver.com>
2023-01-30 22:15:39 +0900

    [Chore] #84 - request 중 필요없는 변수 삭제

--- src/main/java/shop/cazait/domain/user/dto/PostLoginReq.java
+++ src/main/java/shop/cazait/domain/user/dto/PostLoginReq.java
@@ -16,8 +16,6 @@ import javax.validation.constraints.Pattern;
 @Getter
 @ApiModel(value = "PostLoginReq/로그인 정보",description = "회원 로그인시 필요한 회원 정보 dto")
 public class PostLoginReq {
-    @ApiModelProperty(value = "회원 id", example = "1")
-    private Long id;
 
     @ApiModelProperty(value = "이메일", example = "12345@gmail.com")
     @NotNull
--- src/main/java/shop/cazait/domain/user/dto/PostUserReq.java
+++ src/main/java/shop/cazait/domain/user/dto/PostUserReq.java
@@ -17,8 +17,6 @@ import javax.validation.constraints.Pattern;
 @Getter
 @ApiModel(value = "PostUserReq/유저정보",description = "회원 가입시 필요한 dto")
 public class PostUserReq {
-    @ApiModelProperty(value = "회원 id", example = "1")
-    private Long id;
 
     @ApiModelProperty(value = "이메일", example = "12345@gmail.com")
     @Email(message = "이메일 형식을 지키세요.")
@@ -38,7 +36,6 @@ public class PostUserReq {
         this.password = password;
         this.nickname = nickname;
     }
-
 
     public User toEntity(){
         return User.builder()
