6d4d6ade9f004dffa40befc2660838bd0ccf2fc0
Kim Ahyun <kimahyunn132@gmail.com>
2023-01-31 19:05:32 +0900

    [Feat] #83 - 좌표 변환 API 구현 - 3
    Co-authored-by: ParkRootSeok <parkrootseok@gmail.com>

--- src/main/java/shop/cazait/domain/cafe/service/CoordinateService.java
+++ src/main/java/shop/cazait/domain/cafe/service/CoordinateService.java
@@ -1,6 +1,7 @@
 package shop.cazait.domain.cafe.service;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import shop.cazait.domain.cafe.dto.CoordinateVO;
@@ -8,21 +9,21 @@ import shop.cazait.domain.cafe.dto.CoordinateVO;
 @Service
 public class CoordinateService {
 
-    private final String API_KEY = "KakaoAK ";
-    private final String BASE_URL = "https://dapi.kakao.com";
-    private final String URI = "/v2/local/search/address.json";
-    
+    @Value("${rest_api_key}")
+    private String API_KEY;
+    private String baseUrl = "https://dapi.kakao.com";
+    private String uri = "/v2/local/search/address.json";
     public CoordinateVO getCoordinateFromAddress(String address) {
 
         CoordinateVO coordinateVO = WebClient.builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(baseUrl)
                 .build()
                 .get()
-                .uri(builder -> builder.path(URI)
+                .uri(builder -> builder.path(uri)
                         .queryParam("query", address)
                         .build()
                 )
-                .header("Authorization", API_KEY)
+                .header("Authorization", "KakaoAK " + API_KEY)
                 .retrieve()
                 .bodyToMono(CoordinateVO.class)
                 .block();
