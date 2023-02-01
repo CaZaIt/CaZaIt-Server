57d5a0d7dd23f2a1136b6f3bfa3c0a5579ed1e3a
ParkRootSeok <parkrootseok@gmail.com>
2023-02-02 00:23:41 +0900

    [Add] #118 - 서버 Url 추가 및 Consume type 추가

---
+++ src/main/java/shop/cazait/global/config/swagger/SwaggerUrlConfig.java
@@ -0,0 +1,37 @@
+package shop.cazait.global.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+@Component
+public class SwaggerUrlConfig implements WebMvcOpenApiTransformationFilter {
+
+
+    @Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+
+        OpenAPI openApi = context.getSpecification();
+
+        Server devServer = new Server();
+        devServer.setDescription("dev");
+        devServer.setUrl("https://cazait.shop");
+
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("https://localhost:8082");
+
+        openApi.setServers(Arrays.asList(devServer, localServer ));
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType delimiter) {
+        return delimiter.equals(DocumentationType.OAS_30);
+    }
+}
