b543c683350c78279d57b8e570b1172db36199fa
잉퓨 <dayeon620@kakao.com>
2023-02-01 17:23:37 +0900

    [Chore] #80 - JWT 관련 수정파일 적용

--- src/main/java/shop/cazait/global/config/encrypt/Secret.java
+++ src/main/java/shop/cazait/global/config/encrypt/Secret.java
@@ -1,5 +1,6 @@
 package shop.cazait.global.config.encrypt;
 
 public class Secret {
-    public static String USER_INFO_PASSWORD_KEY = "o9pqYVC9-F8_.PEzEiw!L9F6.AYj9jcfVJ*_i.ifXYnyE68kix@Q2dL6rw*bV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-*jnZn";
+	public static String MASTER_INFO_PASSWORD_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTWFzdGVyIn0.Sw6L6Kv_Hf_-f5tKPMZjXGAVACU6daIRNBIyVuYmo2o";
+	public static String USER_INFO_PASSWORD_KEY = "o9pqYVC9-F8_.PEzEiw!L9F6.AYj9jcfVJ*_i.ifXYnyE68kix@Q2dL6rw*bV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-*jnZn";
 }
