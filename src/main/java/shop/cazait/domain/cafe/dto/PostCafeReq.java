36e91bd2be6a6bdec7945495143570e82c786f54
ParkRootSeok <parkrootseok@gmail.com>
2023-02-02 18:14:58 +0900

    [Chore] #123 - CafeImage, Cafe Domain Swagger Annotation 변경

--- src/main/java/shop/cazait/domain/cafe/dto/PostCafeReq.java
+++ src/main/java/shop/cazait/domain/cafe/dto/PostCafeReq.java
@@ -1,19 +1,19 @@
 package shop.cazait.domain.cafe.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import javax.validation.constraints.NotBlank;
 
-@ApiModel(value = "카페 정보 등록 및 수정 Request", description = "카페 등록 및 수정 시 필요한 정보")
+@Schema(description = "카페 정보 등록 및 수정 Request : 카페 등록 및 수정 시 필요한 정보")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostCafeReq {
 
-    @ApiModelProperty(value = "이름", example = "롬곡")
+    @Schema(description = "이름", example = "롬곡")
     @NotBlank(message = "카페 이름을 입력해주세요.")
     private String name;
-    @ApiModelProperty(value = "주소", example = "서울시 중구")
+
+    @Schema(description = "주소", example = "서울시 중구")
     @NotBlank(message = "카페 위치를 입력해주세요.")
     private String address;
 
