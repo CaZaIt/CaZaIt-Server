393a96db83d0be522db3531256af2fcdb482a363
minjun <ijmtylhtf@naver.com>
2023-02-02 17:54:19 +0900

    [Chore] #102 Webconfig swagger 설정 초기화

--- src/main/java/shop/cazait/global/config/encrypt/WebConfig.java
+++ src/main/java/shop/cazait/global/config/encrypt/WebConfig.java
@@ -11,11 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtService jwtService;
     @Override
     public void addInterceptors(InterceptorRegistry reg){
-        reg.addInterceptor(new AuthenticationInterceptor(jwtService))
-                .addPathPatterns("/**")
-                .excludePathPatterns("/swagger-resources/**",
-                                        "/swagger-ui/**",
-                                        "/v3/api-docs");
+        reg.addInterceptor(new AuthenticationInterceptor(jwtService));
     }
 }
 
