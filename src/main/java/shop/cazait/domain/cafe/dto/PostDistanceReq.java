b15f894972526c1a4f215803359003e699dbbd43
Kim Ahyun <kimahyunn132@gmail.com>
2023-02-05 22:16:56 +0900

    [ADD] #157 - PostDistanceReq에 대한 Swagger 설명 추가

--- src/main/java/shop/cazait/domain/cafe/dto/PostDistanceReq.java
+++ src/main/java/shop/cazait/domain/cafe/dto/PostDistanceReq.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Schema(description = "카페와의 거리 Request : 카페와의 거리 계산 시 필요한 정보")
+@Schema(description = "카페와의 거리, 정렬 기준, 제한 거리 Request : 카페와의 거리 계산, 정렬, 제한 거리 내 카페 조회 시 필요한 정보")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostDistanceReq {
@@ -17,10 +17,11 @@ public class PostDistanceReq {
     @Schema(description = "위도", example = "36.987561")
     private String latitude;
 
-    @Schema(description = "정렬 기준", example = "congestion")
+    @Schema(description = "정렬 기준(congestion: 혼잡도순, distance: 거리순)", example = "congestion")
     private String sort;
 
-    @Schema(description = "제한 거리", example = "500")
+    @Schema(description = "제한 거리(0일 때는 모든 카페 출력), 해당 거리 내에 있는 카페 전달, 단위는 m(미터)", example = "500")
+
     private String limit;
 
     @Builder
