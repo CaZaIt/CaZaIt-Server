7900799338ab9425fc4ec2a1f5ae9fabfc9dba0f
ParkRootSeok <parkrootseok@gmail.com>
2023-02-07 00:52:19 +0900

    [Add] #166 - Querydsl 적용

---
+++ src/main/generated/querydsl/shop/cazait/domain/cafe/entity/QCafe.java
@@ -0,0 +1,74 @@
+package shop.cazait.domain.cafe.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QCafe is a Querydsl query type for Cafe
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCafe extends EntityPathBase<Cafe> {
+
+    private static final long serialVersionUID = 293097467L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QCafe cafe = new QCafe("cafe");
+
+    public final shop.cazait.global.common.entity.QBaseEntity _super = new shop.cazait.global.common.entity.QBaseEntity(this);
+
+    public final StringPath address = createString("address");
+
+    public final ListPath<shop.cazait.domain.cafeimage.entity.CafeImage, shop.cazait.domain.cafeimage.entity.QCafeImage> cafeImage = this.<shop.cazait.domain.cafeimage.entity.CafeImage, shop.cazait.domain.cafeimage.entity.QCafeImage>createList("cafeImage", shop.cazait.domain.cafeimage.entity.CafeImage.class, shop.cazait.domain.cafeimage.entity.QCafeImage.class, PathInits.DIRECT2);
+
+    public final shop.cazait.domain.congestion.entity.QCongestion congestion;
+
+    public final QCoordinate coordinate;
+
+    //inherited
+    public final StringPath createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final shop.cazait.domain.master.entity.QMaster master;
+
+    public final StringPath name = createString("name");
+
+    //inherited
+    public final EnumPath<shop.cazait.global.common.status.BaseStatus> status = _super.status;
+
+    //inherited
+    public final StringPath updatedAt = _super.updatedAt;
+
+    public QCafe(String variable) {
+        this(Cafe.class, forVariable(variable), INITS);
+    }
+
+    public QCafe(Path<? extends Cafe> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QCafe(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QCafe(PathMetadata metadata, PathInits inits) {
+        this(Cafe.class, metadata, inits);
+    }
+
+    public QCafe(Class<? extends Cafe> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.congestion = inits.isInitialized("congestion") ? new shop.cazait.domain.congestion.entity.QCongestion(forProperty("congestion"), inits.get("congestion")) : null;
+        this.coordinate = inits.isInitialized("coordinate") ? new QCoordinate(forProperty("coordinate")) : null;
+        this.master = inits.isInitialized("master") ? new shop.cazait.domain.master.entity.QMaster(forProperty("master"), inits.get("master")) : null;
+    }
+
+}
+
---
+++ src/main/generated/querydsl/shop/cazait/domain/cafeimage/entity/QCafeImage.java
@@ -0,0 +1,53 @@
+package shop.cazait.domain.cafeimage.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QCafeImage is a Querydsl query type for CafeImage
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCafeImage extends EntityPathBase<CafeImage> {
+
+    private static final long serialVersionUID = -1557290937L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QCafeImage cafeImage = new QCafeImage("cafeImage");
+
+    public final shop.cazait.domain.cafe.entity.QCafe cafe;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath imageUrl = createString("imageUrl");
+
+    public QCafeImage(String variable) {
+        this(CafeImage.class, forVariable(variable), INITS);
+    }
+
+    public QCafeImage(Path<? extends CafeImage> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QCafeImage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QCafeImage(PathMetadata metadata, PathInits inits) {
+        this(CafeImage.class, metadata, inits);
+    }
+
+    public QCafeImage(Class<? extends CafeImage> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.cafe = inits.isInitialized("cafe") ? new shop.cazait.domain.cafe.entity.QCafe(forProperty("cafe"), inits.get("cafe")) : null;
+    }
+
+}
+
