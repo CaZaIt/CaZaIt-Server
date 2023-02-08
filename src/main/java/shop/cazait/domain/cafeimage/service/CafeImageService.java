4f16c3b583f80d6b3dc26955222b95102b1bd0d2
Kim Ahyun <kimahyunn132@gmail.com>
2023-02-08 18:48:54 +0900

    [FEAT] #183 - S3로 이미지 등록 & 이미지 삭제

--- src/main/java/shop/cazait/domain/cafeimage/service/CafeImageService.java
+++ src/main/java/shop/cazait/domain/cafeimage/service/CafeImageService.java
@@ -3,14 +3,23 @@ package shop.cazait.domain.cafeimage.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import shop.cazait.domain.cafe.entity.Cafe;
 import shop.cazait.domain.cafe.exception.CafeException;
-import shop.cazait.domain.cafeimage.dto.PostCafeImageReq;
+import shop.cazait.domain.cafeimage.dto.GetCafeImageRes;
 import shop.cazait.domain.cafeimage.entity.CafeImage;
 import shop.cazait.domain.cafe.repository.CafeRepository;
 import shop.cazait.domain.cafeimage.repository.CafeImageRepository;
 import shop.cazait.domain.cafeimage.exception.CafeImageException;
+import shop.cazait.domain.master.entity.Master;
+import shop.cazait.domain.master.repository.MasterRepository;
+import shop.cazait.global.common.service.AwsS3Service;
 import shop.cazait.global.error.status.ErrorStatus;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +28,67 @@ public class CafeImageService {
 
     private final CafeRepository cafeRepository;
     private final CafeImageRepository cafeImageRepository;
+    private final MasterRepository masterRepository;
+    private final AwsS3Service awsS3Service;
 
-    public void addCafeImage(Long id, PostCafeImageReq cafeImageReq) throws CafeException {
-        Cafe cafe = cafeRepository.findById(id).orElseThrow(() -> new CafeException(ErrorStatus.INVALID_CAFE_ID));
-        CafeImage cafeImage = CafeImage.builder()
-                .cafe(cafe)
-                .imageUrl(cafeImageReq.getImageUrl())
-                .build();
-        cafeImageRepository.save(cafeImage);
+    public void addCafeImage(Long cafeId, Long masterId, List<MultipartFile> imageFiles) throws CafeException {
+        Cafe cafe = cafeRepository.findById(cafeId).orElseThrow(() -> new CafeException(ErrorStatus.INVALID_CAFE_ID));
+        Master master = masterRepository.findById(masterId).orElseThrow(() -> new CafeException(ErrorStatus.NOT_EXIST_MASTER));
+        if (!(master.getCafe().getId().equals(cafe.getId()))) {
+            throw new CafeException(ErrorStatus.NOT_OPERATE_CAFE);
+        }
+
+        if (imageFiles != null) {
+            List<String> imageUrl = getImageUrl(imageFiles);
+            saveImageUrl(cafe, imageUrl);
+        }
     }
 
-    public void deleteCafeImage(Long id) throws CafeImageException {
-        CafeImage cafeImage = cafeImageRepository.findById(id).orElseThrow(() -> new CafeImageException(ErrorStatus.INVALID_CAFE_IMAGE_ID));
-        cafeImageRepository.deleteById(id);
+    private List<String> getImageUrl(List<MultipartFile> imageFiles) {
+        List<String> imageUrl = imageFiles.stream()
+                .map(imageFile -> {
+                    try {
+                        return awsS3Service.uploadImage(imageFile);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+        return imageUrl;
+    }
+
+    private void saveImageUrl(Cafe cafe, List<String> imageUrl) {
+        List<CafeImage> cafeImages = imageUrl.stream()
+                .map(url -> {
+                    return CafeImage.builder()
+                            .cafe(cafe)
+                            .imageUrl(url)
+                            .build();
+                }).collect(Collectors.toList());
+
+        cafeImages.forEach(cafeImage -> {
+            System.out.println("Save Image Url : " + cafeImage.getImageUrl());
+            cafeImageRepository.save(cafeImage);
+        });
+
+    }
+
+    public List<GetCafeImageRes> readCafeImageList(Long cafeId) {
+        List<CafeImage> cafeImageList = cafeImageRepository.findByCafeId(cafeId);
+        List<GetCafeImageRes> getCafeImageResList = new ArrayList<>();
+        for (CafeImage cafeImage : cafeImageList) {
+            GetCafeImageRes getCafeImageRes = GetCafeImageRes.of(cafeImage);
+            getCafeImageResList.add(getCafeImageRes);
+        }
+        return getCafeImageResList;
+    }
+
+    public void deleteCafeImage(Long cafeImageId, Long masterId) throws CafeImageException {
+        CafeImage cafeImage = cafeImageRepository.findById(cafeImageId).orElseThrow(() -> new CafeImageException(ErrorStatus.INVALID_CAFE_IMAGE_ID));
+        Master master = masterRepository.findById(masterId).orElseThrow(() -> new CafeException(ErrorStatus.NOT_EXIST_MASTER));
+        if (!(master.getCafe().getId().equals(cafeImage.getCafe().getId()))) {
+            throw new CafeException(ErrorStatus.NOT_OPERATE_CAFE);
+        }
+        cafeImageRepository.deleteById(cafeImageId);
     }
 }
