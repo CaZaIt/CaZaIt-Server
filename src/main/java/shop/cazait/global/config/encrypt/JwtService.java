f96077e3f0ccde3d60b5cb080f5e19c51a7e7318
minjun <ijmtylhtf@naver.com>
2023-02-10 02:52:13 +0900

    [Add] #187 - 자신의 id와 같은 토큰으로 api 요청하는지 점검하기 위해 파라미터 없이 토큰에서 userid 추출하는 함수 추가

--- src/main/java/shop/cazait/global/config/encrypt/JwtService.java
+++ src/main/java/shop/cazait/global/config/encrypt/JwtService.java
@@ -9,8 +9,12 @@ import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import shop.cazait.domain.user.exception.UserException;
 import shop.cazait.global.error.exception.BaseException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static shop.cazait.global.error.status.ErrorStatus.*;
 
@@ -19,6 +23,10 @@ import static shop.cazait.global.error.status.ErrorStatus.*;
 public class JwtService {
     private final long ACCESS_TOKEN_VALID_TIME = 30 * 1 * 60 * 1000L;   // 1분
     private final long REFRESH_TOKEN_VALID_TIME = 60 * 60 * 24 * 7 * 1000L;   // 1주
+
+    //테스트 시간
+//    private final long ACCESS_TOKEN_VALID_TIME =  1 * 5 * 1000L;   // 1분
+//    private final long REFRESH_TOKEN_VALID_TIME = 1 * 10 * 1000L;   // 1주
     private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String createJwt(Long userIdx) {
@@ -44,10 +52,44 @@ public class JwtService {
                 .compact();
     }
 
-//    public String getJwt(){
-//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-//        return request.getHeader("X-ACCESS-TOKEN");
-//    }
+    public String getJwt(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return request.getHeader("X-ACCESS-TOKEN");
+    }
+
+    public Long getUserIdx() throws UserException {
+        // JWT 추출
+        String token = getJwt();
+
+        if (token == null || token.length() == 0) {
+            throw new UserException(EMPTY_JWT);
+        }
+        
+        // JWT parsing
+        Jws<Claims> claims;
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+        }
+        catch (ExpiredJwtException exception) {
+            Long userIdx = exception.getClaims().get("userIdx", Long.class);
+            return userIdx;
+        }
+        catch (JwtException exception) {
+            log.error("Token tampered");
+            throw new UserException(INVALID_JWT);
+        }
+        catch (IllegalArgumentException exception) {
+            log.error("Token is null");
+            throw new UserException(EMPTY_JWT);
+        }
+
+        // userIdx 추출
+        return claims.getBody().get("userIdx", Long.class);
+    }
+
 
     public Long getUserIdx(String token) throws UserException {
         log.info("getUseridx token info: "+token);
@@ -118,7 +160,7 @@ public class JwtService {
             log.error("Token Expired UserID : " + exception.getClaims().get("userIdx"));
             return false;
         } catch (JwtException exception) {
-            log.error("Token Tampered");
+            log.error("accessToken Tampered");
             throw new UserException(INVALID_JWT);
         } catch (IllegalArgumentException exception) {
             log.error("Token is null");
@@ -140,7 +182,7 @@ public class JwtService {
             log.error("Token Expired UserID : " + exception.getClaims().get("userIdx"));
             return false;
         } catch (JwtException exception) {
-            log.error("Token Tampered");
+            log.error("refreshToken Tampered");
             throw new UserException(INVALID_JWT);
         } catch (IllegalArgumentException exception) {
             log.error("Token is null");
