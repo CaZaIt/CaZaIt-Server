29aa3c0f425aaa12b8f4beb23339341212d9ddec
minjun <ijmtylhtf@naver.com>
2023-02-10 11:07:08 +0900

    [Del] - 187 불필요한 코드 삭제

--- src/main/java/shop/cazait/domain/user/api/UserApiController.java
+++ src/main/java/shop/cazait/domain/user/api/UserApiController.java
@@ -44,16 +44,6 @@ public class UserApiController {
         return new SuccessResponse<>(postUserRes);
     }
 
-//    @NoAuth
-//    @PostMapping("/log-in")
-//    @ApiOperation(value = "회원 로그인", notes="이메일과 로그인을 통해 로그인을 진행")
-//    public SuccessResponse<PostUserLoginRes> logIn (@RequestBody @Valid PostUserLoginReq postUserLoginReq)
-//            throws UserException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-//        PostUserLoginRes postUserLoginRes = userService.logIn(postUserLoginReq);
-//        return new SuccessResponse<>(postUserLoginRes);
-//    }
-
-
     @GetMapping("/all")
     @ApiOperation(value = "모든 회원을 조회",notes = "회원가입된 모든 회원 정보를 조회")
     public SuccessResponse<List<GetUserRes>> getUsers(){
@@ -89,6 +79,7 @@ public class UserApiController {
     @ApiOperation(value="특정한 회원 정보를 삭제", notes = "자신의 계정 정보를 삭제")
     @ApiImplicitParam (name="userIdx",value = "사용자 userId")
     public SuccessResponse<DeleteUserRes> deleteUser(@PathVariable(name = "userIdx") Long userIdx) {
+
         DeleteUserRes deleteUserRes = userService.deleteUser(userIdx);
         return new SuccessResponse<>(deleteUserRes);
     }
