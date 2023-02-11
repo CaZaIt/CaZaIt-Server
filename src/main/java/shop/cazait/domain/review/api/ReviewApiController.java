4a72d619dd35a7fb7ce8739c59e8101a97c3a3df
swandevson <shai4956@gmail.com>
2023-02-12 01:01:21 +0900

    [Refactor] #196 - Controller 메소드에 thorws 추가

--- src/main/java/shop/cazait/domain/review/api/ReviewApiController.java
+++ src/main/java/shop/cazait/domain/review/api/ReviewApiController.java
@@ -18,12 +18,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import shop.cazait.domain.cafe.exception.CafeException;
 import shop.cazait.domain.review.dto.DelReviewRes;
 import shop.cazait.domain.review.dto.GetReviewRes;
 import shop.cazait.domain.review.dto.PatchReviewReq;
 import shop.cazait.domain.review.dto.PatchReviewRes;
 import shop.cazait.domain.review.dto.PostReviewReq;
 import shop.cazait.domain.review.dto.PostReviewRes;
+import shop.cazait.domain.review.entity.Review;
+import shop.cazait.domain.review.exception.ReviewException;
 import shop.cazait.domain.review.service.ReviewDaoService;
 import shop.cazait.domain.review.service.ReviewProvideService;
 import shop.cazait.domain.user.exception.UserException;
@@ -48,7 +51,8 @@ public class ReviewApiController {
     @GetMapping("/{cafeId}/all")
     public SuccessResponse<List<GetReviewRes>> getReviews(@PathVariable Long cafeId,
                                                           @RequestParam(value = "sortBy", defaultValue = "newest") String sortBy,
-                                                          @RequestParam(value = "score", required = false) Integer score) {
+                                                          @RequestParam(value = "score", required = false) Integer score)
+            throws CafeException, ReviewException {
         List<GetReviewRes> getReviewsRes = reviewProvideService.getReviews(cafeId, sortBy, score);
         SuccessStatus resultStatus = SUCCESS;
 
@@ -61,7 +65,7 @@ public class ReviewApiController {
     @ApiOperation(value = "카페 평점 조회", notes = "카페 ID를 받아 해당 카페의 평점 반환")
     @ApiImplicitParam(name = "cafeId", value = "카페 ID")
     @GetMapping("/{cafeId}/score")
-    public SuccessResponse<Double> getAverageScore(@PathVariable Long cafeId) {
+    public SuccessResponse<Double> getAverageScore(@PathVariable Long cafeId) throws CafeException {
         Double averageScore = reviewProvideService.getAverageScore(cafeId);
 
         return new SuccessResponse<>(SUCCESS, averageScore);
@@ -71,7 +75,7 @@ public class ReviewApiController {
     @ApiOperation(value = "리뷰 하나 조회", notes = "리뷰 ID를 받아 해당 리뷰 조회")
     @ApiImplicitParam(name = "reviewId", value = "리뷰 ID")
     @GetMapping("/{reviewId}")
-    public SuccessResponse<GetReviewRes> getReview(@PathVariable Long reviewId) {
+    public SuccessResponse<GetReviewRes> getReview(@PathVariable Long reviewId) throws ReviewException {
         GetReviewRes getReviewRes = reviewProvideService.getReviewDetail(reviewId);
 
         return new SuccessResponse<>(SUCCESS, getReviewRes);
@@ -86,7 +90,7 @@ public class ReviewApiController {
     public SuccessResponse<PostReviewRes> addReview(@PathVariable Long userId,
                                                     @PathVariable Long cafeId,
                                                     @RequestBody @Valid PostReviewReq postReviewReq)
-            throws UserException {
+            throws UserException, CafeException, ReviewException {
         PostReviewRes postReviewRes = reviewDaoService.addReview(userId, cafeId, postReviewReq);
         return new SuccessResponse<>(CREATE_REVIEW, postReviewRes);
     }
@@ -94,14 +98,15 @@ public class ReviewApiController {
     @ApiOperation(value = "리뷰 수정", notes = "리뷰 ID를 받아 해당 리뷰 점수 및 내용 수정")
     @PatchMapping("/edit/{reviewId}")
     public SuccessResponse<PatchReviewRes> updateReview(@PathVariable Long reviewId,
-                                                        @RequestBody @Valid PatchReviewReq patchReviewReq) {
+                                                        @RequestBody @Valid PatchReviewReq patchReviewReq)
+            throws ReviewException {
         PatchReviewRes patchReviewRes = reviewDaoService.updateReview(reviewId, patchReviewReq);
         return new SuccessResponse<>(SUCCESS, patchReviewRes);
     }
 
     @ApiOperation(value = "리뷰 삭제", notes = "리뷰 ID를 받아 해당 리뷰 삭제")
     @DeleteMapping("/{reviewId}")
-    public SuccessResponse<DelReviewRes> deleteReview(@PathVariable Long reviewId) {
+    public SuccessResponse<DelReviewRes> deleteReview(@PathVariable Long reviewId) throws ReviewException {
         DelReviewRes delReviewRes = reviewDaoService.deleteReview(reviewId);
         return new SuccessResponse<>(SUCCESS, delReviewRes);
     }
