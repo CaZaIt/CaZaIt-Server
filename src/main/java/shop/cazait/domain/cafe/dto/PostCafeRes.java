75ac5b44998c5600a51c25719948fafe8e19fe62
Kim Ahyun <kimahyunn132@gmail.com>
2023-02-12 21:46:59 +0900

    [ADD] #199 - 카페 등록, 수정에 대한 Response dto 생성

---
+++ src/main/java/shop/cazait/domain/cafe/dto/PostCafeRes.java
@@ -0,0 +1,36 @@
+package shop.cazait.domain.cafe.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import shop.cazait.domain.cafe.entity.Cafe;
+import shop.cazait.domain.cafeimage.dto.GetCafeImageRes;
+import shop.cazait.domain.congestion.entity.CongestionStatus;
+
+import java.util.List;
+@Schema(description = "카페 등록 및 수정 Response : 카페 등록 및 수정 후 받는 응답")
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+public class PostCafeRes {
+    @Schema(description = "카페 ID", example = "1")
+    private Long cafeId;
+    @Schema(description = "혼잡도 상태", example = "FREE")
+    private CongestionStatus congestionStatus;
+    @Schema(description = "이름", example = "롬곡")
+    private String name;
+    @Schema(description = "위치", example = "서울특별시 광진구 군자동 광나루로17길 18")
+    private String address;
+    @Schema(description = "이미지 url")
+    private List<GetCafeImageRes> getCafeImageRes;
+
+    public static PostCafeRes of(Cafe cafe, List<GetCafeImageRes> getCafeImageResList) {
+        return PostCafeRes.builder()
+                .cafeId(cafe.getId())
+                .congestionStatus(cafe.getCongestion().getCongestionStatus())
+                .name(cafe.getName())
+                .address(cafe.getAddress())
+                .getCafeImageRes(getCafeImageResList)
+                .build();
+    }
+}
