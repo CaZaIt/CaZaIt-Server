347f244f6be268e214362119512207bef20be5b2
swandevson <shai4956@gmail.com>
2023-02-13 15:53:52 +0900

    [Del] #202 - 불필요한 구문 삭제

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,11 +11,6 @@ TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-cp $PROJECT_ROOT/application.yml application.yml
-cp $PROJECT_ROOT/application-aws.yml application-aws.yml
-cp $PROJECT_ROOT/application-dev.yml application-dev.yml
-cp $PROJECT_ROOT/application-kakao.yml application-kakao.yml
 
 # 환경 변수 설정
 source ~/.bashrc
