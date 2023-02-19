be1aec008917380bf34b068bc281a6ea01ad8b18
swandevson <43706436+swandevson@users.noreply.github.com>
2023-02-19 23:03:21 +0900

    [Fix] 어플리케이션명 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/cazait-server.jar"
+JAR_FILE="$PROJECT_ROOT/cazait.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
