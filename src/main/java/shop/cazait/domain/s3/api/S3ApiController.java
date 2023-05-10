afb1ca4732cf86722c0b9341e3aab59400b9d958
parkrootseok <parkrootseok@gmail.com>
2023-05-10 16:14:07 +0900

    [Chore] #268 - PreSignedUrl DTO 네이밍 변경

--- src/main/java/shop/cazait/domain/s3/api/S3ApiController.java
+++ src/main/java/shop/cazait/domain/s3/api/S3ApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import shop.cazait.domain.s3.dto.GetPreSignedUrlRes;
+import shop.cazait.domain.s3.dto.response.PreSignedUrlCreateOutDTO;
 import shop.cazait.domain.s3.service.S3Service;
 import shop.cazait.global.common.dto.response.SuccessResponse;
 import shop.cazait.global.config.encrypt.NoAuth;
@@ -26,7 +26,7 @@ public class S3ApiController {
     @GetMapping("/pre-signed")
     @Operation(summary = "미리 서명된 주소 생성", description = "이미지 업로드를 위해 필요한 미리 서명된 주소를 받는다.")
     @Parameter(name = "directory", description = "이미지 종류", example = "cafe")
-    public SuccessResponse<GetPreSignedUrlRes> createPreSignedUrl(@RequestParam String directory) {
+    public SuccessResponse<PreSignedUrlCreateOutDTO> createPreSignedUrl(@RequestParam String directory) {
         return new SuccessResponse<>(SuccessStatus.SUCCESS, s3Service.createPreSignedUrl(directory));
     }
 
