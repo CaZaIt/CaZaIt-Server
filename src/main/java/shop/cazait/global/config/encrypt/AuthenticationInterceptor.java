610cf9d31340d68ac643e0aa7a2d5018054aa907
minjun <ijmtylhtf@naver.com>
2023-06-20 20:32:20 +0900

    [Add] #275 - 인터셉터에서의 userID 검증 불필요한 코드 삭제 및 리팩토링

--- src/main/java/shop/cazait/global/config/encrypt/AuthenticationInterceptor.java
+++ src/main/java/shop/cazait/global/config/encrypt/AuthenticationInterceptor.java
@@ -5,9 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.objenesis.strategy.BaseInstantiatorStrategy;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
 import shop.cazait.domain.user.exception.UserException;
 import shop.cazait.global.error.exception.BaseException;
 
@@ -15,6 +18,8 @@ import javax.persistence.Basic;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -27,10 +32,12 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     private final JwtService jwtService;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws UserException{
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
         System.out.println("request.pathinfo = " + request.getPathInfo());
-        System.out.println("request.getRequestURI() = " + request.getRequestURI());
-        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("request = " + request);
+        System.out.println("request.getServletPath() = " + request.getServletPath());
+        System.out.println("handler = " + handler);
 //        if(BasicErrorController.class == handlerMethod.getBeanType()){
 //            return true;
 //        }
@@ -42,37 +49,26 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
         String accessToken = request.getHeader("Authorization");
         log.info("AccessToken in interceptor prehandle = "+accessToken);
 
-//        if(jwtService.isValidToken(accessToken)){
-//            return true;
-//        }
-//        else {
-//            return false;
-//        }
-        final Map<String, String> pathVariables = (Map<String, String>) request
+      /*  final Map<String, String> pathVariables = (Map<String, String>) request
                 .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
         System.out.println("pathVariables = " + pathVariables);
 
         Optional<String> masterId = Optional.ofNullable(pathVariables.get("masterId"));
-        Optional<String> userId = Optional.ofNullable(pathVariables.get("userId"));
+        Optional<String> userId = Optional.ofNullable(pathVariables.get("userId"));*/
 
         if(jwtService.isValidToken(accessToken)) {
-            if (!masterId.isEmpty()) {
-                jwtService.isValidAccessTokenId(Long.valueOf(masterId.get()));
-            }
-            if (!userId.isEmpty()) {
-                jwtService.isValidAccessTokenId(Long.valueOf(userId.get()));
-            }
+          return true;
         }
         else {
             return false;
         }
-        return true;
     }
 
 
     private boolean checkAnnotation(Object handler,Class cls){
-       
         HandlerMethod handlerMethod=(HandlerMethod) handler;
+        System.out.println("handlerMethod.getBean() = " + handlerMethod.getBean());
+        System.out.println("handlerMethod = " + handlerMethod);
         System.out.println("handlerMethod.getMethodAnnotation(cls) = " + handlerMethod.getMethodAnnotation(cls));
         if(handlerMethod.getMethodAnnotation(cls)!=null){ //해당 어노테이션이 존재하면 true.
             return true;
