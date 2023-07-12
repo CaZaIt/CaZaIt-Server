1ed63158a208bc131f680bed4655a478d149dc66
minjun <ijmtylhtf@naver.com>
2023-07-12 12:23:15 +0900

    [Refactor] #301 - 마스터 정보 수정 DTO 수정

--- src/main/java/shop/cazait/domain/master/dto/request/MasterUpdateInDTO.java
+++ src/main/java/shop/cazait/domain/master/dto/request/MasterUpdateInDTO.java
@@ -5,22 +5,29 @@ import javax.validation.constraints.Pattern;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Schema(description = "마스터 수정 Request : 수정할 마스터 계정 정보")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class MasterUpdateInDTO {
 
-	@Schema(description = "Master 이메일")
-	@NotBlank(message = "수정하고자 하는 마스터 계정의 이메일을 입력해주세요.")
-	private String email;
+	@Schema(description = "Master 아이디")
+	@NotBlank(message = "수정하고자 하는 마스터 계정의 아이디를 입력해주세요.")
+	private String idNumber;
 
 	@Schema(description = "Master 패스워드")
 	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는최소 8자리에 숫자, 문자, 특수문자 각 1개 이상 포함하여 사용하세요.")
 	@NotBlank(message = "수정하고자 하는 마스터 계정의 패스워드를 입력해주세요.")
 	private String password;
+
+	@Schema(description = "Master 전화번호")
+	@NotBlank(message = "수정하고자 하는 마스터 계정의 휴대폰 전화번호를 입력해주세요.")
+	private String phoneNumber;
 
 	@Schema(description = "Master 닉네임")
 	@NotBlank(message = "수정하고자 하는 마스터 계정의 닉네임을 입력해주세요.")
