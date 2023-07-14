8c657e7afd0ded0cec7e5bf0c185208fef89d719
minjun <ijmtylhtf@naver.com>
2023-07-14 12:28:30 +0900

    [Refactor] #253 - UUID id로 토큰에 있는 id와 pathvariable에 있는 id 비교

--- src/main/java/shop/cazait/global/config/encrypt/AuthenticationInterceptor.java
+++ src/main/java/shop/cazait/global/config/encrypt/AuthenticationInterceptor.java
@@ -18,12 +18,10 @@ import javax.persistence.Basic;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static shop.cazait.global.error.status.ErrorStatus.INVALID_JWT;
+import static shop.cazait.global.error.status.ErrorStatus.INVALID_REQUEST;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -50,7 +48,11 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
         log.info("AccessToken in interceptor prehandle = "+accessToken);
 
         if(jwtService.isValidToken(accessToken)) {
-          return true;
+            if(verifyTokenUserId(request)){
+                return true;
+            }else{
+                return false;
+            }
         }
         else {
             return false;
@@ -67,6 +69,40 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             return true;
         }
         return false;
+    }
+
+    /**유효한 토큰을 발급 받았지만 토큰 userid, masterid와 다른 userid, masterid에 접근하는 것을 방지**/
+    public boolean verifyTokenUserId(HttpServletRequest request) throws UserException {
+        //헤더 토큰에 있는 userid 추출
+        String jwtFromHeader = jwtService.getJwtFromHeader();
+        UUID userIdxInToken = jwtService.getUserIdx(jwtFromHeader);
+
+        //pathvariable인 userid추출
+        final Map<String, String> pathVariables = (Map<String, String>) request
+                .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        Optional<String> masterIdInPathVariable = Optional.ofNullable(pathVariables.get("masterId"));
+        Optional<String>  userIdInPathVariable= Optional.ofNullable(pathVariables.get("userId"));
+
+        //1. userid 존재시
+        if(userIdInPathVariable.isPresent()){
+            String stringUserId = userIdInPathVariable.get();
+            UUID userId = UUID.fromString(stringUserId);
+            if(userId.equals(userIdxInToken)){
+                return true;
+            }else {
+                throw new UserException(INVALID_REQUEST);
+            }
+        } else if (masterIdInPathVariable.isPresent()) {
+            String stringMasterId = masterIdInPathVariable.get().toString();
+            UUID masterId = UUID.fromString(stringMasterId);
+            if(masterId.equals(userIdxInToken)){
+                return true;
+            }else{
+                throw new UserException(INVALID_REQUEST);
+            }
+        }
+        return true;
     }
 }
 
