6b57d5d483f5f0c2eda177f074c627692f33da5f
minjun <ijmtylhtf@naver.com>
2023-07-28 15:23:10 +0900

    [Chore] #317 - DTO 명 수정

--- src/main/java/shop/cazait/domain/user/dto/UserEnterPasswordInResetPasswordInDTO.java
+++ src/main/java/shop/cazait/domain/user/dto/UserEnterPasswordInResetPasswordInDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Schema(description = "유저 비밀번호 수정 Request : 비밀번호 재설정")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserUpdatePasswordInDTO {
+public class UserEnterPasswordInResetPasswordInDTO {
 
     @Schema(description = "비밀번호 재설정 문자 발송 및 인증이 완료된 전화번호", example = "01012345678")
     private String userPhoneNumber;
