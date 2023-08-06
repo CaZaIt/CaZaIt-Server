b87a4d52c1a0583c9414d007269f593d89d0e683
swandevson <shai4956@gmail.com>
2023-08-06 16:57:57 +0900

    [Fix] 리뷰 작성 및 조회 DTO 필드 변경

--- src/main/java/shop/cazait/domain/review/dto/ReviewGetOutDTO.java
+++ src/main/java/shop/cazait/domain/review/dto/ReviewGetOutDTO.java
@@ -6,29 +6,28 @@ import lombok.Builder;
 import lombok.Getter;
 import shop.cazait.domain.review.entity.Review;
 
-import java.util.UUID;
 
 
 @Schema(description = "리뷰 하나 조회 Response : 해당 리뷰의 정보")
 @Getter
 @Builder(access = AccessLevel.PRIVATE)
 public class ReviewGetOutDTO {
-    @Schema(description = "유저 ID")
-    private UUID userId;
+    @Schema(description = "카페명")
+    private String cafeName;
 
-    @Schema(description = "카페 ID")
-    private Long cafeId;
+    @Schema(description = "리뷰 작성자명(닉네임)")
+    private String nickname;
 
-    @Schema(description = "점수")
+    @Schema(description = "리뷰 점수")
     private Integer score;
 
-    @Schema(description = "내용")
+    @Schema(description = "리뷰 내용")
     private String content;
 
     public static ReviewGetOutDTO of(Review review) {
         return ReviewGetOutDTO.builder()
-                .userId(review.getUser().getId())
-                .cafeId(review.getCafe().getId())
+                .cafeName(review.getCafe().getName())
+                .nickname(review.getUser().getNickname())
                 .score(review.getScore())
                 .content(review.getContent())
                 .build();
