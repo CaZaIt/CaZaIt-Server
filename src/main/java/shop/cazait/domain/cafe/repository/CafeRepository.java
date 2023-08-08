e6a920f8269fa0192794927ad2afb838148ffc92
swandevson <shai4956@gmail.com>
2023-08-09 01:41:04 +0900

    [Fix] Review의 cafeId type Long -> UUID로 변경

--- src/main/java/shop/cazait/domain/cafe/repository/CafeRepository.java
+++ src/main/java/shop/cazait/domain/cafe/repository/CafeRepository.java
@@ -1,17 +1,18 @@
 package shop.cazait.domain.cafe.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.cazait.domain.cafe.model.entity.Cafe;
-
-import java.util.List;
 import shop.cazait.domain.master.model.entity.Master;
 import shop.cazait.global.common.status.BaseStatus;
 
-public interface CafeRepository extends JpaRepository <Cafe, UUID> {
 
-    List<Cafe >findAllByStatus(BaseStatus status);
+
+public interface CafeRepository extends JpaRepository<Cafe, UUID> {
+
+    List<Cafe> findAllByStatus(BaseStatus status);
 
     List<Cafe> findByNameContainingIgnoreCaseAndStatus(String name, BaseStatus status);
 
--- src/main/java/shop/cazait/domain/review/repository/CustomReviewRepository.java
+++ src/main/java/shop/cazait/domain/review/repository/CustomReviewRepository.java
@@ -1,12 +1,13 @@
 package shop.cazait.domain.review.repository;
 
 import java.util.List;
+import java.util.UUID;
 import shop.cazait.domain.review.entity.Review;
 
 
 
 public interface CustomReviewRepository {
-    List<Review> findNewestPageByCafeId(Long cafeId, Integer score, Long lastId, int size);
+    List<Review> findNewestPageByCafeId(UUID cafeId, Integer score, Long lastId, int size);
 
-    List<Review> findOldestPageByCafeId(Long cafeId, Integer score, Long lastId, int size);
+    List<Review> findOldestPageByCafeId(UUID cafeId, Integer score, Long lastId, int size);
 }
