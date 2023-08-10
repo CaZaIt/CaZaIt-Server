a10b9081b5c75357181c719e7b6bbc14a3b6f531
minjun <ijmtylhtf@naver.com>
2023-08-10 15:44:03 +0900

    [Add] #337 - 비밀번호 변경 시 유저 검증 DTO

---
+++ src/main/java/shop/cazait/domain/user/dto/UserVerifyUserInfoInResetPasswordInDTO.java
@@ -0,0 +1,19 @@
+package shop.cazait.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Schema(description = "비밀번호 유저 정보 검증 Request : 아이디와 전화번호가 일치하는지 검증")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserVerifyUserInfoInResetPasswordInDTO {
+    @Schema(description = "아이디", example = "cazait1234")
+    private String accountName;
+    @NotBlank
+    @Schema(description = "전화번호", example = "01012345678")
+    private String phoneNumber;
+}
---
+++ src/main/java/shop/cazait/domain/user/dto/UserVerifyUserInfoInResetPasswordOutDTO.java
@@ -0,0 +1,29 @@
+package shop.cazait.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import shop.cazait.domain.user.entity.User;
+
+@Schema(description = "비밀번호 유저 정보 검증 Request : 아이디와 전화번호가 일치하는 경우")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+public class UserVerifyUserInfoInResetPasswordOutDTO {
+
+    @NotBlank
+    @Schema(description = "아이디", example = "cazait1234")
+    private String accountName;
+
+    @NotBlank
+    @Schema(description = "전화번호", example = "01012345678")
+    private String phoneNumber;
+
+    public static UserVerifyUserInfoInResetPasswordOutDTO of(User user){
+        return UserVerifyUserInfoInResetPasswordOutDTO.builder()
+                .accountName(user.getAccountName())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
+}
