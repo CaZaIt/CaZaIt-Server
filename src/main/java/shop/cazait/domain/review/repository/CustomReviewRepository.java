f93a58c9dd7e237f5ce2c01451ae4bde14f60e54
swandevson <shai4956@gmail.com>
2023-08-15 23:49:14 +0900

    [Fix] Review custom repository 인터페이스 수정

--- src/main/java/shop/cazait/domain/review/repository/CustomReviewRepository.java
+++ src/main/java/shop/cazait/domain/review/repository/CustomReviewRepository.java
@@ -1,13 +1,4 @@
 package shop.cazait.domain.review.repository;
 
-import java.util.List;
-import java.util.UUID;
-import shop.cazait.domain.review.entity.Review;
-
-
-
 public interface CustomReviewRepository {
-    List<Review> findNewestPageByCafeId(UUID cafeId, Integer score, Long lastId, int size);
-
-    List<Review> findOldestPageByCafeId(UUID cafeId, Integer score, Long lastId, int size);
 }
