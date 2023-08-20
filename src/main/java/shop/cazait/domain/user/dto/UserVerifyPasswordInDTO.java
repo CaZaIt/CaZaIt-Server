eb1a55ec0ee9bbf99db686fb81f30bf6948ba676
minjun <ijmtylhtf@naver.com>
2023-08-20 19:56:28 +0900

    [Add] #349 - 계정 정보 관리 페이지 비밀번호 검증 Request, Response DTO

---
+++ src/main/java/shop/cazait/domain/user/dto/UserVerifyPasswordInDTO.java
@@ -0,0 +1,23 @@
+package shop.cazait.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "계정정보관리 페이지에서의 비밀번호 검증 Request : 현재 로그인한 유저의 비밀번호가 적절한지")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserVerifyPasswordInDTO {
+
+    @Schema(description = "로그인한 회원의 id")
+    private UUID id;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는최소 8자리에 숫자, 문자, 특수문자 각 1개 이상 포함하여 사용하세요.")
+    @NotBlank
+    @Schema(description = "회원이 입력한 비밀번호", example = "abc12345#!")
+    private String password;
+}
