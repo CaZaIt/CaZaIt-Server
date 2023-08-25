fe14aa05eefbc0c4bbd4edf1b85848763d23444e
swandevson <shai4956@gmail.com>
2023-08-25 21:09:28 +0900

    [Fix] 리뷰 조회에 userId response 추가

--- src/main/java/shop/cazait/domain/review/dto/ReviewGetOutDTO.java
+++ src/main/java/shop/cazait/domain/review/dto/ReviewGetOutDTO.java
@@ -1,10 +1,13 @@
 package shop.cazait.domain.review.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import shop.cazait.domain.cafe.model.entity.Cafe;
 import shop.cazait.domain.review.entity.Review;
+import shop.cazait.domain.user.entity.User;
 
 
 
@@ -12,6 +15,9 @@ import shop.cazait.domain.review.entity.Review;
 @Getter
 @Builder(access = AccessLevel.PRIVATE)
 public class ReviewGetOutDTO {
+    @Schema(description = "유저 ID")
+    private UUID userId;
+
     @Schema(description = "카페명")
     private String cafeName;
 
@@ -25,9 +31,13 @@ public class ReviewGetOutDTO {
     private String content;
 
     public static ReviewGetOutDTO of(Review review) {
+        User user = review.getUser();
+        Cafe cafe = review.getCafe();
+
         return ReviewGetOutDTO.builder()
-                .cafeName(review.getCafe().getName())
-                .nickname(review.getUser().getNickname())
+                .userId(user.getId())
+                .cafeName(cafe.getName())
+                .nickname(user.getNickname())
                 .score(review.getScore())
                 .content(review.getContent())
                 .build();
